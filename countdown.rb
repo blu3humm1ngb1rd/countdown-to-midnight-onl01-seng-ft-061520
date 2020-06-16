7134b1838c2e271c99174927a1955eb3cd0fb2a6
BJ Lilley <bethjeanlilley@gmail.com>
2020-06-16 22:31:41 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
 #write your code here
 
-def countdown
+def countdown(number)
+ 
+  while number > 0 
+
+  puts "#{number} SECONDS(S)!"
+  
+  number -= 1
+
+  end 
+  "HAPPY NEW YEAR!"
 end
+
+
+
+def countdown_with_sleep(number = 10)
+    while number > 0 
+
+  puts "#{number} SECONDS(S)!"
+  
+  number -= 1
+  sleep 1
+  end 
+  "HAPPY NEW YEAR!"
